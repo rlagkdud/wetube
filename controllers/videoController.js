a8f7a75bb12eaddca67e96f3d67049154054b44d
Hayeong Kim <ha09368@gmail.com>
2020-06-18 13:27:10 +0900

    more controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import {videos} from "../db"
+import routes from "../routes"
 
 export const home = (req, res) =>{
     res.render("home", { pageTitle: "Home", videos});
@@ -13,11 +14,20 @@ export const search = (req, res) => {
     res.render("search",{ pageTitle: "Search", searchingBy/*: searchingBy*/, videos});
 };
 
-export const upload = (req, res) => 
+export const getUpload = (req, res) => {
     res.render("Upload",{ pageTitle: "Upload" });
+};
+
+export const postUpload = (req, res) => {
+    const{
+        body:{ file,title,description }
+    } = req;
+    //todo: upload and save video
+    res.redirect(routes.videoDetail(332493));
+};
 
 export const videoDetail = (req, res) => 
-    res.render("Video detail",{ pageTitle: "Video Detail" });
+    res.render("videoDetail",{ pageTitle: "Video Detail" });
 
 export const editVideo = (req, res) => 
     res.render("editVideo",{ pageTitle: "Edit Video" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
 import routes from "../routes"
-import {upload, editVideo, deleteVideo, videoDetail } from "../controllers/videoController";
+import { editVideo, deleteVideo, videoDetail, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload,upload);
-videoRouter.get(routes.videoDetail,videoDetail);
+videoRouter.get(routes.upload,getUpload);
+videoRouter.post(routes.upload,postUpload);
+
+
+videoRouter.get(routes.videoDetail(),videoDetail);
 videoRouter.get(routes.editVideo,editVideo);
 videoRouter.get(routes.deleteVideo,deleteVideo);
 
