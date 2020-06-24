69b4395eecdd96e96502ba6c9f2d866cfdecd165
Hayeong Kim <ha09368@gmail.com>
2020-06-24 15:49:55 +0900

    searching videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,13 +11,22 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req; //const searchingBy = req.query.term;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", {
     pageTitle: "Search",
     searchingBy /*: searchingBy*/,
+    videos,
   });
 };
 
