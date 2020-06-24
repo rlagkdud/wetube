3335268fcf73d3656fcaa747f0e7fec9bae91fe8
Hayeong Kim <ha09368@gmail.com>
2020-06-24 15:27:04 +0900

    intalling ESlint

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ import routes from "./routes";
 const app = express();
 
 app.use(helmet());
-app.set('view engine',"pug");
-app.use("/uploads",express.static("uploads"));
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
-app.use(routes.home,globalRouter);
-app.use(routes.users,userRouter);
-app.use(routes.videos,videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
