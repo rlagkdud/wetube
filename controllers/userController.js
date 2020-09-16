6d9fd935723b718d72aeb85ebdb6ca57b778a580
rlagkdud <ha0936861@gmail.com>
2020-09-16 16:50:05 +0900

    local authentication with passport2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,47 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
-    res.render("join",{ pageTitle: "Join" });
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
-    const {
-        body:{ name, email, password, password2 }
-    } = req;
-    if(password !== password2){
-        res.status(400);
-        res.render("join",{ pageTitle: "Join" });
-    }else{
-        // Todo: register user
-        //todo: log user in
-        res.redirect(routes.home);
+export const postJoin = async (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
     }
-    
+    //todo: log user in
+    res.redirect(routes.home);
+  }
 };
 
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
-export const getLogin = (req, res) => 
-    res.render("login",{ pageTitle: "Login" });
 export const postLogin = (req, res) => {
-    res.redirect(routes.home);
+  res.redirect(routes.home);
 };
 
-export const logout  = (req, res) => {
-    //todo: process logout
-    res.redirect(routes.home);
+export const logout = (req, res) => {
+  //todo: process logout
+  res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) => 
-res.render("userDetail",{ pageTitle: "User Detail" });
-export const editProfile = (req, res) => 
-res.render("editProfile",{ pageTitle: "Edit Profile" });
-export const changePassword = (req, res) => 
-res.render("changePassword",{ pageTitle: "Change Password" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
--- init.js
+++ init.js
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
 import "./models/Comment";
