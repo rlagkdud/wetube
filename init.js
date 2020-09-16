943980e081fc6e9e84b911f08025de2d40c6730e
rlagkdud <ha0936861@gmail.com>
2020-09-16 15:41:36 +0900

    local authentication with Passport1

--- init.js
+++ init.js
@@ -4,12 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
-
 const handleListening = () =>
-    console.log(`✅Listening on : http://localhost:${PORT}`);
+  console.log(`✅Listening on : http://localhost:${PORT}`);
 
-
-app.listen(PORT,handleListening);
+app.listen(PORT, handleListening);
